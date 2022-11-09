4783df4d5916bc75fec16c76a1796dfd9df8ae22
Vincent <v3rdugo@yahoo.com>
2022-11-09 15:12:58 -0800

    create product route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Products } from './routes/Products'
+import { ProductCreate } from './routes/ProductCreate';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Products/>}>
-
-        </Route>
+        <Route path="/" element={<Products/>}/>
+        <Route path="/create" element={<ProductCreate/>}/>
       </Routes>
     </BrowserRouter>
   );
